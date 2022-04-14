35fdfe93e3bc1db390de4d4d2589d76488b86eef
Tesfawen <lijleda092@gmail.com>
2022-04-14 09:33:31 +0000

    my commit message on 9th task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+
+int p = 100;
+int i;
+
+i = 1;
+while (i <= p)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ")
+}
+else if (i % 5 == 0)
+{
+if (i < 0)
+printf("Buzz ");
+
+else
+printf("Buzz");
+}
+else
+{
+printf("%i ", i);
+}
+
+i++;
+}
+printf("\n");
+return (0);
+}

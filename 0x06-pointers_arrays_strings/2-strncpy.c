5b627637ce2edc07377b5f4e78bb3620583b550b
Tesfawen <lijleda092@gmail.com>
2022-04-21 13:53:49 +0000

    fixing the bugs

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,11 @@
 /**
- *_strncpy - copies a string
- *@dest: A pointer to a character that will be changed
- *@src: A pointer to a character that will be changed
- *@n: value
- *Return: dest
+ * _strncpy - function that
+ *            copies a string.
+ * @dest: A pointer to a character that will be changed
+ * @src: A pointer to a character that will be changed
+ * @n: value
+ *
+ * Return: @dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -12,6 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 
 i = 0;
+
 while (i< n && src[i] != '\0')
 {
 dest[i] = src[i];

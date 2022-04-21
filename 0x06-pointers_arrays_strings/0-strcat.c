32d34d9ea868d75411cb2e9debf61a0c385bd69c
Tesfawen <lijleda092@gmail.com>
2022-04-21 09:29:55 +0000

    Fixing the bugs

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,22 @@
 #include "main.h"
 /**
- *_strcat - concatenates two strings
- *@dest: A pointer to a character that will be changed
- *@src: Apointer to a character that will be updated
- *Return: dest
+ * _strcat - concatenates two strings
+ * @dest: A pointer to a character that will be changed
+ * @src: Apointer to a character that will be updated
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+int c, c2;
+c = 0;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
+while (dest[c])
+c++;
 
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
+for (c2 = 0; src[c2]; c2++)
+
+dest[c++] = src[c2];
 
 return (dest);
 }

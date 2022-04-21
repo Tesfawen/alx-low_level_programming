51d4e9bf19ade038950818ee69985b296e5647b0
Tesfawen <lijleda092@gmail.com>
2022-04-21 13:56:18 +0000

    fixing the bugs

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,5 @@
-#include "main.h"
 /**
- *_strcmp - compares to a character that will be changed
- *s1: A pointer to a character that will be changed
- *@s2: A pointer to character that will be modified
- *Return: dest
+ * _strcmp - compares to a character that will be changed
+ * s1: A pointer to a character that will be changed
+ * @s2: A pointer to character that will be modified
+ * Return: @dest

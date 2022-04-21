0d1acf659041bae970eead2f43293824cfbbe340
Tesfawen <lijleda092@gmail.com>
2022-04-21 18:57:52 +0000

    fixing the bugs

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,18 +4,32 @@
  * reverse_array - reverse content an array of integers
  * @a: array ofintegers
  * @n: number of element of array
- * Return: Always 0 (success)
+ * Return: void
  */
 
 void reverse_array(int *a, int n)
 {
-int x, y;
+int *start_c, *end_c, c;
+int i;
 
-for (x = 0; x < (n / 2); x++)
+start_c = a;
+end_c = a;
+
+for (i = 0; i < n - 1; i++)
+{
+end_c++;
+}
+
+
+for (i = 0; i < n / 2; i++)
 {
 
-y = a[x];
-a[x] = a[n - x - 1);
-a[n - x - 1] = y;
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+
+start_c++;
+end_c--;
 }
+
 }

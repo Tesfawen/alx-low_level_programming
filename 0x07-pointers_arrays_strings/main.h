3160f634378576b2a18fbdda36e6d496763d7209
Tesfawen <lijleda092@gmail.com>
2022-04-26 04:57:21 +0000

    My main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,1 +1,10 @@
 char *_memset(char *s, charr b, unsigned int n);
+char *_memcpy(char *dest, chat *src, unsigned int n);
+char *_strchar(char *s, char c);
+unsigned int _strspn(char *s, char *accept):
+char *_strpbrk(char *s, char *accept);
+char *ststr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+

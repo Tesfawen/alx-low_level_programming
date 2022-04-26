7a14157930d7556eac6bd2ec94da0c686e2668de
Tesfawen <101813048+Tesfawen@users.noreply.github.com>
2022-04-26 16:15:23 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef H_MAIN_H
+# define H_MAIN_H
+
 char *_memset(char *s, charr b, unsigned int n);
 char *_memcpy(char *dest, chat *src, unsigned int n);
 char *_strchar(char *s, char c);
@@ -6,5 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *ststr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+#endif
 
 

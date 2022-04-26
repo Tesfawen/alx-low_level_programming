c578b642390b56d790abd7a16e973a182b035b19
Tesfawen <lijleda092@gmail.com>
2022-04-26 20:25:04 +0000

    Advanced

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * ste_string - sets the value of a pointer to a char
+ * @s: content is to be copied
+ * @to: string
+ * Return: void
+ */
+
+void set_string(char **s, char*to)
+{
+*s = to;
+}
+
+100-set_string.c
+
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: content is to be copied
+ * @to: string
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
+

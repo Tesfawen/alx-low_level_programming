35808179deac39e8d3f8685db5311f13d27f68bf
Tesfawen <101813048+Tesfawen@users.noreply.github.com>
2022-04-26 16:23:38 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,14 +1,26 @@
-#ifndef H_MAIN_H
-# define H_MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
-char *_memset(char *s, charr b, unsigned int n);
-char *_memcpy(char *dest, chat *src, unsigned int n);
-char *_strchar(char *s, char c);
-unsigned int _strspn(char *s, char *accept):
+/**
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *ststr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
 #endif
 
 

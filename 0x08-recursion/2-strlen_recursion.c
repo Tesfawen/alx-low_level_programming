5f8b980476282f46b53e764308eb2ce732717ade
Tesfawen <lijleda092@gmail.com>
2022-04-28 12:23:53 +0000

    Fixing the bugs

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,9 @@
  */
 int _strlen_recursion(char *s);
 {
-int x;
-
-x = 0;
 if (*s == '\0')
 return (0);
 
-x = 1 + _strlen_recursion(s + 1);
-
-return (x);
+else
+return (1 + _strlen_recursion(s + 1));
 }

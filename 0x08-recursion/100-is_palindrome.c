8086188bb1d9ecc10a005bcfede14896318c58a4
Tesfawen <101813048+Tesfawen@users.noreply.github.com>
2022-04-28 17:08:51 +0300

    Update 100-is_palindrome.c
    My8th task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,3 +1,4 @@
+  
 #include "main.h"
 
 /**
@@ -8,31 +9,32 @@
 
 int _strlen_recursion(char *s)
 {
-if (!*s)
-{
-return (0);
-}
-return (1 + _strlen_recursion(++s));
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
 }
 
 /**
- *p1 - palindrome
+ * p1 - palindrome
  * @s: pointer to string
- * @1: position
+ * @l: position
  * Return: boolena
  */
 
 int p1(char *s, int l)
 {
-if (l < 1)
-{
-return (1);
-}
-if (*s == *(s + 1))
-{
-return (p1(s + 1, l - 2));
-}
-return (0);
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
 }
 
 /**
@@ -43,7 +45,7 @@ return (0);
 
 int is_palindrome(char *s)
 {
-int len = _strlen_recursion(s);
+	int len = _strlen_recursion(s);
 
-return (p1(s, len - 1));
+	return (p1(s, len - 1));
 }

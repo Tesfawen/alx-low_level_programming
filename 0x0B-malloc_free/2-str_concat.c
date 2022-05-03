3af8428e8ec22fba27deec5cdd78b49e2a013025
Tesfawen <lijleda092@gmail.com>
2022-05-03 13:58:30 +0000

    Fixing the bugs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -41,7 +41,7 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-size = (_strlen(s1) + _strlen(s2) + 1;
+size = (_strlen(s1) + _strlen(s2) + 1);
 
 dst = (char *)malloc(size * sizeof(char));
 
@@ -50,7 +50,7 @@ if (dst == 0)
 return (NULL);
 }
 
-for )i = 0; *(s1 + i) != '\0'; i++)
+for (i = 0; *(s1 + i) != '\0'; i++)
 *(dst + i) = *(s1 + i);
 
 for (j = 0; *(s1 + j) != '\0'; j++)

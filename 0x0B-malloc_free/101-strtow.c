54abc8db6688fba64e411bcddf46cd41c2ac47bd
Tesfawen <lijleda092@gmail.com>
2022-05-03 20:00:51 +0000

    fixing the bugs

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,39 +7,39 @@
  */
 char **strtow(char *str)
 {
-	char **ptr;
-	int i, k, len, start, end, j = 0;
-	int words =  countWords(str);
+char **ptr;
+int i, k, len, start, end, j = 0;
+int words =  countWords(str);
 
-	if (!str || !countWords(str))
-		return (NULL);
-	ptr = malloc(sizeof(char *) * (words + 1));
-	if (!ptr)
-		return (NULL);
-	for (i = 0; i < words; i++)
-	{
-		start = startIndex(str, j);
-		end = endIndex(str, start);
-		len = end - start;
-		ptr[i] = malloc(sizeof(char) * (len + 1));
-		if (!ptr[i])
-		{
-			i -= 1;
-			while (i >= 0)
-			{
-				free(ptr[i]);
-					i--;
-			}
-			free(ptr);
-			return (NULL);
-		}
-		for (k = 0; k < len; k++)
-			ptr[i][k] = str[start++];
-		ptr[i][k++] = '\0';
-		j = end + 1;
-	}
-	ptr[i] = NULL;
-	return (ptr);
+if (!str || !countWords(str))
+return (NULL);
+ptr = malloc(sizeof(char *) * (words + 1));
+if (!ptr)
+return (NULL);
+for (i = 0; i < words; i++)
+{
+start = startIndex(str, j);
+end = endIndex(str, start);
+len = end - start;
+ptr[i] = malloc(sizeof(char) * (len + 1));
+if (!ptr[i])
+{
+i -= 1;
+while (i >= 0)
+{
+free(ptr[i]);
+i--;
+}
+free(ptr);
+return (NULL);
+}
+for (k = 0; k < len; k++)
+ptr[i][k] = str[start++];
+ptr[i][k++] = '\0';
+j = end + 1;
+}
+ptr[i] = NULL;
+return (ptr);
 }
 
 /**

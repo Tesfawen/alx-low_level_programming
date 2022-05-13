291f43e088d98a1831eea1367c23a2bf2898267f
Tesfawen <lijleda092@gmail.com>
2022-05-13 19:48:47 +0000

    Fixing

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,12 @@
  * print_name - prints a name
  * @name: name to print
  * @f: pointer to the printing function
+ *
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+if (!name || !f)
+return;
+f(name);
 }
